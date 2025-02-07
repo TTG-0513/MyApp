0d200b644ccdc7a6448195b38517c61684dc2616
TTG-0513 <viper1305@freenet.de>
2025-02-07 11:06:04 +0100

    check_up_7.02.2025

--- lib/features/authentication/widgets/my_app_theme.dart
+++ lib/features/authentication/widgets/my_app_theme.dart
@@ -4,25 +4,25 @@ import 'package:ldj_app/features/game_selection/models/color_app.dart';
 
 class MyAppTheme {
   ThemeData get theme => ThemeData(
-        textTheme: GoogleFonts.manrope(),
-        copyWith(),
-        primaryColor: ColorApp().primaryColor,
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: ColorApp().firered,
-          unselectedItemColor: ColorApp().black,
-        ),
-        textButtonTheme: TextButtonThemeData(
-            style: ButtonStyle(
-          foregroundColor: WidgetStateColor.resolveWith(
-            (_) => ColorApp().black,
-          ),
-        )),
-        filledButtonTheme: FilledButtonThemeData(
-            style: ButtonStyle(
-          backgroundColor: WidgetStateColor.resolveWith(
-            (_) => ColorApp().black,
-          ),
-        )),
+      //       textTheme: GoogleFonts.manrope(),
+      //     copyWith(),
+      //   primaryColor: ColorApp().primaryColor,
+      // bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      // backgroundColor: ColorApp().firered,
+      //         unselectedItemColor: ColorApp().black,
+      //     ),
+      //   textButtonTheme: TextButtonThemeData(
+      //     style: ButtonStyle(
+      // foregroundColor: WidgetStateColor.resolveWith(
+//            (_) => ColorApp().black,
+      //        ),
+      //    )),
+      //  filledButtonTheme: FilledButtonThemeData(
+      //    style: ButtonStyle(
+      //backgroundColor: WidgetStateColor.resolveWith(
+      //           (_) => ColorApp().black,
+      //       ),
+      //   )),
       );
   ThemeData get darkTheme => ThemeData(
         brightness: Brightness.dark,
